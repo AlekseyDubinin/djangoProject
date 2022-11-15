8c6262751f06eedfa6343b515fc3468cb8d3a9b6
AlekseyDubinin <113135230+AlekseyDubinin@users.noreply.github.com>
2022-11-15 16:01:55 +0100

    Delete ads directory

--- ads/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from ads.models import Categories, Ad, Location, User
-
-
-admin.site.register(Categories)
-admin.site.register(Location)
-admin.site.register(Ad)
-admin.site.register(User)
